4fa0fdcddb5dc022f66ca88bd46248d34d7f5414
Teller501 <ande777j@stud.kea.dk>
2023-04-25 11:58:03 +0200

    initial commit

---
+++ src/main/java/dk/kea/projectmanagement/ProjectManagementApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.projectmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectManagementApplication.class, args);
+    }
+
+}
