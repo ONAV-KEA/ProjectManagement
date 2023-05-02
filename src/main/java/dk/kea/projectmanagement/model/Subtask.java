8dcbc5d37b968e2f8992fa04785e2a572d60075f
OmarKayed <omar2346@stud.kea.dk>
2023-05-02 12:00:44 +0200

    Added attributes to Subtask

--- src/main/java/dk/kea/projectmanagement/model/Subtask.java
+++ src/main/java/dk/kea/projectmanagement/model/Subtask.java
@@ -1,4 +1,109 @@
 package dk.kea.projectmanagement.model;
 
+import java.time.LocalDate;
+
 public class Subtask {
+    private int id;
+    private String title;
+    private String description;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private int assigneeId;
+    private int cost;
+    private String status;
+    private String comment;
+    private int taskId;
+
+    public Subtask(int id, String title, String description, LocalDate startDate, LocalDate endDate, int assigneeId, int cost, String status, String comment, int taskId) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.assigneeId = assigneeId;
+        this.cost = cost;
+        this.status = status;
+        this.comment = comment;
+        this.taskId = taskId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public int getAssigneeId() {
+        return assigneeId;
+    }
+
+    public void setAssigneeId(int assigneeId) {
+        this.assigneeId = assigneeId;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public int getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(int taskId) {
+        this.taskId = taskId;
+    }
 }
