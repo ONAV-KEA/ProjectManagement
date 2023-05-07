5f9614a8a50321201bb274438047be9ff1d7e835
Teller501 <ande777j@stud.kea.dk>
2023-05-07 11:52:51 +0200

    added SubtaskFormDTO and setup button for creating subtask in project.html

---
+++ src/main/java/dto/SubtaskFormDTO.java
@@ -0,0 +1,75 @@
+package dto;
+
+import java.time.LocalDate;
+
+public class SubtaskFormDTO {
+    private int id;
+    private String title;
+    private String description;
+    private LocalDate startDate;
+    private double cost;
+    private int projectId;
+
+    public SubtaskFormDTO(int id, String title, String description, LocalDate startDate,double cost, int projectId) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.startDate = startDate;
+        this.cost = cost;
+        this.projectId = projectId;
+    }
+
+    public SubtaskFormDTO() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+
+
+    public int getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(int projectId) {
+        this.projectId = projectId;
+    }
+}
