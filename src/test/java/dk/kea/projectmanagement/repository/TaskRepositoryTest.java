ad91edfd125128c23c532b1f523831ecda2c104f
nicolaiandersson <nico602d@stud.kea.dk>
2023-05-23 16:41:47 +0200

    move TaskRepositoryTest and UserRepositoryTest to test repository folder

--- src/test/java/dk/kea/projectmanagement/repository/TaskRepositoryTest.java
+++ src/test/java/dk/kea/projectmanagement/repository/TaskRepositoryTest.java
@@ -1,4 +1,4 @@
-package dk.kea.projectmanagement;
+package dk.kea.projectmanagement.repository;
 
 import dk.kea.projectmanagement.dto.TaskAndSubtaskDTO;
 import dk.kea.projectmanagement.model.Subtask;
