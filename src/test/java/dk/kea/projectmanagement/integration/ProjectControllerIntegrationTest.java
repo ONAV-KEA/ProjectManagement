062501e0ea76a3c0c09c4ab098b6a139f02f2004
nicolaiandersson <nico602d@stud.kea.dk>
2023-05-26 23:34:58 +0200

    full CRUD test of project controller done. (only thing left in here is that the user that is set up before the test need to also be deleted after)

--- src/test/java/dk/kea/projectmanagement/integration/ProjectControllerIntegrationTest.java
+++ src/test/java/dk/kea/projectmanagement/integration/ProjectControllerIntegrationTest.java
@@ -9,24 +9,33 @@ import dk.kea.projectmanagement.service.UserService;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import dk.kea.projectmanagement.service.ProjectService;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.time.LocalDate;
 
+import static org.junit.Assert.assertNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -37,6 +46,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 @ActiveProfiles("test")
 public class ProjectControllerIntegrationTest {
     @Autowired
@@ -47,6 +57,9 @@ public class ProjectControllerIntegrationTest {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     // We mock the session object
     MockHttpSession session = new MockHttpSession();
@@ -68,27 +81,17 @@ public class ProjectControllerIntegrationTest {
 
     @AfterEach
     public void tearDown() throws Exception {
-        User user = (User) session.getAttribute("user");
-        if (user != null) {
-            int userId = user.getId();
-            if (userId > 0) {
-                try {
-                    userService.deleteUser(userId);
-                } catch (RuntimeException e) {
-                    throw new Exception("Failed to delete user with ID: " + userId, e);
-                }
-            }
-        }
+
     }
 
     @Test
-    public void ProjectCreationAndViewTest() throws Exception {
+    @Rollback
+    public void projectCrudTest() throws Exception {
         // We prepare a project and set some values for it - name and dates
         Project form = new Project();
         form.setName("Test Project");
         form.setStartDate(LocalDate.now());
         form.setEndDate(LocalDate.now().plusDays(7));
-
 
         // We go to the /createproject and the prepared project formula is entered
         this.mockMvc.perform(post("/createproject").session(session)
@@ -108,6 +111,31 @@ public class ProjectControllerIntegrationTest {
         // Which lets us set the id variable in the project/ url to the project and see if it works correctly
         this.mockMvc.perform(get("/project/" + projectId).session(session))
                 .andExpect(status().isOk());
+
+        // Prepare data formula that will be used to update the project with
+        Project updatedForm = new Project();
+        updatedForm.setName("Updated Test Project");
+        updatedForm.setStartDate(LocalDate.now().plusDays(1));
+        updatedForm.setEndDate(LocalDate.now().plusDays(8));
+
+        // We perform the project update with the updatedForm data, and then redirect user back to dashboard
+        this.mockMvc.perform(post("/project/" + projectId + "/projectsettings").session(session)
+                        .flashAttr("project", updatedForm)
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED))
+                .andExpect(status().isFound())
+                .andExpect(redirectedUrl("/project/" + projectId));
+
+        // Then check to see if the project has been updated with the updatedForm
+        Project updatedProject = projectService.getProjectById(projectId);
+        Assertions.assertEquals("Updated Test Project", updatedProject.getName());
+        Assertions.assertEquals(LocalDate.now().plusDays(1), updatedProject.getStartDate());
+        Assertions.assertEquals(LocalDate.now().plusDays(8), updatedProject.getEndDate());
+
+
+        // Finally, we delete the project
+        this.mockMvc.perform(get("/project/" + projectId + "/deleteproject").session(session))
+                .andExpect(status().isFound())
+                .andExpect(redirectedUrl("/projects"));
     }
 }
 
